d096f8717a6f830348e2390ef25d913063ff5ee0
sgadde <gaddesurendrakumar@gmail.com>
2020-03-08 21:20:22 +0530

    Added mamny to many relation Between Recipe and Category

--- src/main/java/gadde/springframework/recipe/domain/Recipe.java
+++ src/main/java/gadde/springframework/recipe/domain/Recipe.java
@@ -28,6 +28,11 @@ public class Recipe {
     private Notes notes;
     @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
     private Set<Ingredient> ingredients;
+    @ManyToMany()
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
