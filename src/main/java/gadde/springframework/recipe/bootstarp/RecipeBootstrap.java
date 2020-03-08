f19e247c8112c9d124a935eb077eeed5b32e5c1e
sgadde <gaddesurendrakumar@gmail.com>
2020-03-09 01:02:04 +0530

    Fixed bidirectional entity setting problem on parent

--- src/main/java/gadde/springframework/recipe/bootstarp/RecipeBootstrap.java
+++ src/main/java/gadde/springframework/recipe/bootstarp/RecipeBootstrap.java
@@ -121,7 +121,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "\n" +
                 "\n" +
                 "Read more: http://www.simplyrecipes.com/recipes/perfect_guacamole/#ixzz4jvoun5ws");
-        guacNotes.setRecipe(guacRecipe);
+       // guacNotes.setRecipe(guacRecipe);
         guacRecipe.setNotes(guacNotes);
 
         //very redundent - could add helper method, and make this simpler
@@ -174,7 +174,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "\n" +
                 "\n" +
                 "Read more: http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/#ixzz4jvu7Q0MJ");
-        tacoNotes.setRecipe(tacosRecipe);
+        //tacoNotes.setRecipe(tacosRecipe);
         tacosRecipe.setNotes(tacoNotes);
 
         tacosRecipe.addIngredient(new Ingredient("Ancho Chili Powder", new BigDecimal(2), tableSpoonUom));
